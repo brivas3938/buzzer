ad735c10f4bf077ad9f432aa77f20fc362cc19e8
brivas3938 <81707869+brivas3938@users.noreply.github.com>
2021-03-31 11:14:22 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+if (input.buttonIsPressed(Button.AB)) {
+    music.playTone(988, music.beat(BeatFraction.Whole))
+} else if (input.buttonIsPressed(Button.A)) {
+    music.playTone(131, music.beat(BeatFraction.Whole))
+} else if (input.buttonIsPressed(Button.B)) {
+    music.playTone(370, music.beat(BeatFraction.Whole))
+}
